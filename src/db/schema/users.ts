d4fec75f1a827e4e551a35c3655894fab37a58f0
devarsheecodess <devarsheegaunekar29@gmail.com>
2025-07-18 00:09:07 +0530

    Mongo config

---
+++ src/db/schema/users.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchena = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const users =
+  mongoose.models.users || mongoose.model("users", userSchena);
